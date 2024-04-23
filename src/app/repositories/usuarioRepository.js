b8f82ed26a4c5f8cdeeb07cfb5815069b79349a4
Cristianocxm <114105719+Cristianocxm@users.noreply.github.com>
2024-04-23 15:49:42 -0300

    Add files via upload

--- src/app/repositories/usuarioRepository.js
+++ src/app/repositories/usuarioRepository.js
@@ -1,5 +1,4 @@
 import mongoose from '../database/conexao.js';
-import { consulta } from '../database/conexao.js'
 import Usuario from '../../models/usuarios.js'
 
 
@@ -17,33 +16,6 @@ class usuarioRepository{
             throw erro;
           });
       }
-
-
-      findAll() {
-        const sql = "SELECT * FROM bdusers.usuarios;"
-        return consulta(sql, 'Não foi possível encontrar os dados.')
-    }
-
-    findById(id) {
-        const sql = "SELECT * FROM bdusers.usuarios WHERE id = ?;"
-        return consulta(sql, id, 'Não foi possível localizar o id informado.')
-    }
-
-    create(usuario) {
-        const sql = "INSERT INTO usuarios SET ?;"
-        return consulta(sql, usuario, 'Não foi possível realizar o cadastro.')
-    }
-
-    update(usuario, id) {
-        const sql = "UPDATE usuarios SET ? where id = ?;"
-        return consulta(sql, [usuario, id], 'Não foi possível atualizar os dados.')
-    }
-
-    delete(id) {
-        const sql = "DELETE FROM bdusers.usuarios WHERE id = ?;"
-        return consulta(sql, id, 'Não foi possível excluir o cdastro.')
-    }
-
 }
 
 export default new usuarioRepository()
